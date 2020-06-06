d5e6cbc089abffc785a8c62a162eb121301a9a0c
MrPengueeen <aonmoy0.0@gmail.com>
2020-06-06 22:47:08 +0600

    Dynamic fontSize change based on the number

--- main.js
+++ main.js
@@ -1,12 +1,21 @@
 window.onload = function() {
-    value = 100;
-    if (value > 1000) {
-        document.getElementById("number").style.fontSize = "100px";
-    }
+    value = 1;
+    fontSize = changeFontSize(value);
     document.getElementById("number").innerHTML = formatNumber(value);
+    document.getElementById("number").style.fontSize = fontSize.toString() + "px";
    
 }
 
 function formatNumber(num) {
     return num.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,'); //Formatted number with commas
+}
+
+function changeFontSize(num) {
+    fontSize = 200;
+    while(num != 0) {
+        fontSize -= 20;
+        num = Math.floor(num/10);
+        console.log(num); /// Debugging purpose
+    }
+    return fontSize;
 }
