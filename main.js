7f09c9d2bb0f93bbcec26f72ffab0a75bd22568d
Mahbubur Rahman <mahbubcu14@gmail.com>
2020-06-06 23:14:31 +0600

    Changes.... Added setInterval

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+
 window.onload = function() {
     value = 1;
     fontSize = changeFontSize(value);
@@ -6,9 +7,24 @@ window.onload = function() {
    
 }
 
-function formatNumber(num) {
-    return num.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,'); //Formatted number with commas
+setInterval(chageNumber, 1000);
+
+function chageNumber(){
+    curNum=document.getElementById("number").textContent;
+    value=parseInt(curNum)*10;
+    if(value>99999999){
+        value=20;
+    }
+    console.log(value);
+    document.getElementById("number").innerHTML = value;
+    fontSize = changeFontSize(value);
+    document.getElementById("number").style.fontSize = fontSize.toString() + "px";
+
 }
+
+// function formatNumber(num) {
+//     return num.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,'); //Formatted number with commas
+// }
 
 function changeFontSize(num) {
     fontSize = 200;
