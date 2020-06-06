3544026cd9a234f6a4da0733c17ab0893c5ed0a2
MrPengueeen <aonmoy0.0@gmail.com>
2020-06-07 00:07:07 +0600

    everything into index.html

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
 
 window.onload = function() {
-    value = 1;
+    value = 99999999;
     fontSize = changeFontSize(value);
     document.getElementById("number").innerHTML = formatNumber(value);
     document.getElementById("number").style.fontSize = fontSize.toString() + "px";
    
 }
 
-setInterval(chageNumber, 1000);
+setInterval(changeNumber, 1000);
 
-function chageNumber(){
+function changeNumber(){
     curNum=document.getElementById("number").textContent;
     value=parseInt(curNum)*10;
     if(value>99999999){
@@ -22,14 +22,14 @@ function chageNumber(){
 
 }
 
-// function formatNumber(num) {
-//     return num.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,'); //Formatted number with commas
-// }
+function formatNumber(num) {
+    return num.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,'); //Formatted number with commas
+}
 
 function changeFontSize(num) {
-    fontSize = 200;
+    fontSize = 140;
     while(num != 0) {
-        fontSize -= 20;
+        fontSize -= 11.8;
         num = Math.floor(num/10);
         console.log(num); /// Debugging purpose
     }
